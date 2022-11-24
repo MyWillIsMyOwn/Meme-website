e5d4374e25e51d919f21fb2cf8765328f70160d8
Filip <weyerfilip@gmail.com>
2022-11-24 13:36:53 +0100

    pagination

--- web/main/models.py
+++ web/main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Photos(models.Model):
-    title = models.CharField(max_length=45)
-    picture = models.TextField()
+    title = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to="images")
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         managed = False
-        db_table = 'Photos'
+        db_table = "Photos"
--- web/main/views.py
+++ web/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Photos
+from django.core.paginator import Paginator
+
 
 def index(response):
-    photos = Photos.objects.all() #zasysa wszystkie dane z bazy danych
+    photos = Photos.objects.all()  # zasysa wszystkie dane z bazy danych
+
+    pagination = Paginator(photos, 2)
+    page = response.GET.get("page")
+    single_page = pagination.get_page(page)
     context = {
-        'photos': photos,
+        "photos": photos,
+        "single_page": single_page,
     }
     return render(response, "main/base.html", context)
